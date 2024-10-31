0660c59f16038bfbb1fe1b67325ec416d7b4968d
Mohsin <mohsinhp918@gmail.com>
2024-10-31 16:42:41 +0500

    miner change

--- src/chrischen/Portfolio section/Portfolio.js
+++ src/chrischen/Portfolio section/Portfolio.js
@@ -5,10 +5,15 @@ import { portfolioData } from "../Data files/data";
 const Portfolio = () => {
   return (
     <section className="portfolio_section" id="portfolio">
-      <h1 className="heading1"  data-aos="fade-up"
-          data-aos-delay="150"
-          data-aos-duration="1000"
-          data-aos-easing="ease-in-out">Portfolio</h1>
+      <h1
+        className="heading1"
+        data-aos="fade-up"
+        data-aos-delay="150"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+      >
+        Portfolio
+      </h1>
       {portfolioData.map((portfolioData, i) => (
         <PortfolioSlider portfolioData={portfolioData} key={i} />
       ))}
